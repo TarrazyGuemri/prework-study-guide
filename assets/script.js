01d1df9d8fd0cd1e2b8997a9e8d7ff99ba81f194
TarrazyGuemri <tarrazyguemri@outlook.com>
2022-12-02 10:49:14 +0000

    created a for loop for an array of the different topics learnt

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+
+for (var x=0; x < topics.length; x++) {
+    console.log(topics[x])
+}
+
+
+/*
+if (topic === 'HTML') {
+    console.log("Lets Study HTML!");
+}   else if (topic === 'CSS') {
+    console.log("Lets Study CSS");
+}   else if (topic === 'Git') {
+    console.log("Lets Study Git");
+}   else if (topic === 'JavaScript') {
+    console.log("Lets Study JavaScript");
+}   else {
+    console.log('Please Try Again!');
+}
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) {
+console.log(shapes[x]);
+}
+*/
